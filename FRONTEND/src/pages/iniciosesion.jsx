8d4d4af02a6320c84bcc32154d8752f70395d3a2
Karol <karolbarrero51@gmail.com>
2026-05-18 22:35:42 -0500

    Vista inicio de sesion

---
+++ FRONTEND/src/pages/iniciosesion.jsx
@@ -0,0 +1,43 @@
+import FormularioAuth from "../components/Auth";
+import logo from "../assets/logo/Mocha_s_blossom-removebg-preview.png";
+import img from "../assets/Decoraciones/Screenshot_2025-09-13_194328-removebg-preview.png"
+
+function InicioSesion(){
+
+const campos=[
+
+{
+tipo:"email",
+id:"email",
+placeholder:"Email",
+label:"Email"
+},
+
+{
+tipo:"password",
+id:"password",
+placeholder:"Contraseña",
+label:"Contraseña"
+}
+
+];
+
+return(
+
+<FormularioAuth
+titulo="Iniciar Sesión"
+subtituloKR="인생은 케이크 같아요"
+subtituloES="La vida es como un pastel"
+campos={campos}
+textoBoton="Sign in"
+textoInferior="¿No tienes cuenta?"
+textoLink="Registrarse"
+ruta="/inicio"
+imagen={img}
+logo={logo}
+/>
+
+)
+}
+
+export default InicioSesion
